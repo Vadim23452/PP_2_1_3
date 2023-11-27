fda34d1ffb7825c17ccb509e2a1b088bf3b22bf5
Vadim <vadim23452@yandex.ru>
2023-11-27 13:39:28 +0300

    исправленно

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,5 +1,6 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -7,13 +8,25 @@ import org.springframework.stereotype.Component;
 public class AnimalsCage {
 
     @Autowired
-    private Animal animal;
+    @Qualifier("cat")
+    private Animal animalCat;
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animalDog;
+
+    @Autowired
+    private Timer time;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(animalCat.toString() + " " + animalDog.toString());
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return time;
     }
 }
